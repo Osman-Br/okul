86104c218ef2083cbe72964c04abb3aed80ecc7e
Osman-Br <56321113+Osman-Br@users.noreply.github.com>
2020-11-24 13:07:12 +0300

    Update veriYapilariOdev3.c

--- VeriYapilari/veriYapilariOdev3.c
+++ VeriYapilari/veriYapilariOdev3.c
@@ -3,60 +3,65 @@
 #include <stdbool.h>
 
 struct Stack{
-    int index;
+    int top;
     int size;
     int* array;
 };
 
 struct Stack* createStack(int size){
     struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
-    stack->index = -1;
+    stack->top = -1;
     stack->size = size;
     stack->array = (int*)malloc(sizeof(int)*size);
+    for(int i = 0; i < size; i++){
+        stack->array[i] = -1;
+    }
     return stack;
 }
 
 bool isFull(struct Stack* stack){
-    if(stack->size-1 == stack->index)
+    if(stack->size-1 == stack->top)
         return true;
     else
         return false;
+}
+
+bool isEmpty(struct Stack *stack) {
+    if (stack->top == -1)
+        return true;
+    else
+        return false;
+
 }
 
 void add(struct Stack* stack, int element){
     if(isFull(stack))
         return;
-    stack->array[++stack->index] = element;
-}
-
-bool isEmpty(struct Stack *stack) {
-    if (stack->index == -1)
-        return true;
-    else
-        return false;
-
+    else if(!isEmpty(stack) && stack->array[stack->top] < element){
+        return;
+    }
+    stack->array[++stack->top] = element;
 }
 
 void eraseLast(struct Stack* stack){
     if(isEmpty(stack))
         return;
-    stack->array[stack->index] = -1;
-    stack->index -= 1;
+    stack->array[stack->top] = -1;
+    stack->top -= 1;
 }
 
 void moveToOtherStack(struct Stack* source, struct Stack* destination)
 {
-    int temp = source->array[source->index];
+    int temp = source->array[source->top];
     eraseLast(source);
     add(destination, temp);
-
 }
 
 void printStacks(struct Stack* stack1,struct Stack* stack2,struct Stack* stack3){
     struct Stack* stackArray[] = {stack1,stack2,stack3};
     for(int j = 0 ; j < 3 ; j++) {
         for (int i = stackArray[j]->size-1; i > -1 ; i--) {
-            printf("%d.cubuk %d.kat disk : %d\n",j+1, i, stackArray[j]->array[i]);
+            printf("%d.cubuk %d.kattaki disk capi : %d\n",j+1, i, stackArray[j]->array[i]);
         }
     }
 }
@@ -70,7 +75,7 @@ int main(){
     add(stick1, 2);
     add(stick1, 1);
 
-    printf("-------------------siralanmadan once--------------------\n\n");
+    printf("-------------------SIRALANMADAN ONCE--------------------\n");
     printStacks(stick1,stick2,stick3);
 
     moveToOtherStack(stick1,stick3);
@@ -81,7 +86,7 @@ int main(){
     moveToOtherStack(stick2,stick3);
     moveToOtherStack(stick1,stick3);
 
-    printf("-------------------siralandiktan sonra-----------------\n\n");
+    printf("\n-------------------SIRALANDIKTAN SONRA-----------------\n");
     printStacks(stick1,stick2,stick3);
 
     return 0;
